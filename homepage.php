cebebca25665cc67558b73fabdccea94cdf499d1
AndrieUST <andriepaul.macatangay.cics@ust.edu.ph>
2024-01-26 20:08:20 +0800

    Started Homepage.

--- homepage.php
+++ homepage.php
@@ -3,11 +3,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Homepage</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <!-- Login CSS -->
-    <link href="./login.css" rel="stylesheet" type="text/css"/>
+    <link href="./homepage.css" rel="stylesheet" type="text/css"/>
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
@@ -15,12 +15,10 @@
 </head>
 <body>
 <header class="topnav">
-        <div class="topnav-left">
         <a target="_blank" href="">
           <img align = "left" class = "ld-icon" src="LDAssets/lz logo.png" alt="LazyDaze">
         </a>
-        </div>
   </header>
-  
+
 	</body>
 </html>
