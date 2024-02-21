3bdb19461abeabd849071ea75015c1bab76a62cd
AndrieUST <andriepaul.macatangay.cics@ust.edu.ph>
2024-02-21 13:12:28 +0800

    Removed info icon in inquiries.

---
+++ .php_tmp/homepage.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- Homepage CSS -->
+    <link href="./homepage.css" rel="stylesheet" type="text/css"/>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <!-- Background Image -->
+<div class = "bg">
+  <!-- Navigation Bar -->
+<header class="topnav">
+        <a href="">
+          <img align = "left" class = "ld-icon" src="LDAssets/lz logo.png" alt="LazyDaze">
+        </a>
+  </header>
+  <div class="intro">
+  <h2>What are you waiting for?</h2>
+  <h1>Get your Lazy Daze now</h1>
+  <a href="login.php">
+  <button class = "go-login" name = "go-to-login" value = "Go-Login">Login</button>
+</a>
+<h3>or</h3>
+<a href="mainpage.php">
+  <button class = "go-mainpage" name = "go-to-mainpage" value = "Go-Mainpage">View Products</button>
+</a>
+</div>
+</div>	
+</body>
+</html>
