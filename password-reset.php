75ae6300730468ef1a04e564dcba2787dd18dab7
MarkUserGit <124044202+MarkUserGit@users.noreply.github.com>
2024-03-04 00:19:59 +0800

    Database for password reset
    added a database for the reset password. It requires email verification code. I will use phpmailer. Token needs to be fix first in myphpadmin

--- password-reset.php
+++ password-reset.php
@@ -2,6 +2,48 @@
 
 include('connect.php');
 
+function send_password_reset($get_email,$token)
+{
+    
+}
+
+
+if(isset($_POST ['email_input']))
+{
+$email = mysqli_real_escape_string($conn, $_POST['Customer_Email']);
+$token = md5(rand());
+
+$check_email = "SELECT * FROM users WHERE Customer_Email ='$email' LIMIT 1";
+$check_email_run = mysqli_query($conn, $check_email);
+
+if(mysqli_num_rows($check_email_run) > 0)
+{
+    $row = mysqli_fetch_array($check_email_run);
+    $get_email = $row['Customer_Email'];
+
+    $update_token = "UPDATE users SET verify_token = '$token' WHERE Customer_Email='$get_email' LIMIT 1";
+    $update_token_run = mysqli_query($conn, $update_token);
+    if($update_token_run)
+    {
+        send_password_reset($get_email,$token);
+        $_SESSION['status'] = "We e-emailed you a password reset link";
+        header("Location: password-reset.php");
+        exit(0);
+
+    }
+    else
+    {
+        $_SESSION['status'] = "Something went wrong. #1";
+        header("Location: password-reset.php");
+        exit(0);
+    }
+}
+{
+    $_SESSION['status'] = "No Email Found";
+    header("Location: password-reset.php");
+    exit(0);
+}
+}
 
 ?>
 
