85c87bb8bf4c545137fbe8b1f6a2571608c01ee3
AndrieUST <andriepaul.macatangay.cics@ust.edu.ph>
2024-03-05 22:04:22 +0800

    Almost done with cart page. (Progress bar to be added.)
    Known issues:
    - Quantity Button doesn't work on other items except the first

---
+++ cart.php
@@ -0,0 +1,149 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Lazy Daze!</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- Homepage CSS -->
+    <link href="./cart.css" rel="stylesheet" type="text/css"/>
+    <!-- FontAwesome Icons CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- Website Icon -->
+    <link rel="icon" href="./LDAssets/lz logo.png">
+</head>
+<body>
+  <!-- Background Image -->
+<div class = "bg">
+  <!-- Navigation Bar -->
+<header class="topnav">
+        <a href="">
+          <img align = "left" class = "ld-icon" src="LDAssets/lz logo.png" alt="LazyDaze">
+        </a>
+        <!-- Icons -->
+        <div class="topnav-right">
+            <!-- Logout Icon -->
+            <div class="nav-icon">
+                <a href="logout.php">
+                    <i class="fa-solid fa-arrow-right-from-bracket fa-xl"></i>
+                </a>
+            </div>
+            <div class="nav-line"></div>
+            <!-- Reviews Icon -->
+            <div class="nav-icon">
+                <a href="reviews.php">
+                    <i class="fa-solid fa-star fa-xl"></i>
+                </a>
+            </div>
+            <div class="nav-line"></div>
+            <!-- Info Icon -->
+            <div class="nav-icon">
+                <a href="inquiries.php">
+                    <i class="fa-solid fa-circle-info fa-xl"></i>
+                </a>
+            </div>
+            <div class="nav-line"></div>
+        </div>
+  </header>
+  <!-- Cart Header -->
+  <section class="section-2"></section>
+  <!-- Cart -->
+  <section class = "container">
+  <form id="cart-form" name="cart-form" method="get">
+        <div class="grid">
+            <div class="row">
+                    <!-- Cart Row -->
+                    <div class="h-layout h-flex-block">
+                        <a href="#" class="rm-btn">
+                            <i class="fa-solid fa-xmark fa-2xl"></i>
+                        </a>
+                        <img src="LDAssets/sample-shirt.png" alt="Shirt" class="item-image" />
+                        <!-- Product Details Column -->
+                        <div class="v-layout v-flex-block">
+                            <h3>NINJA from MANILA Tee - Black</h3>
+                            <select class="select-field" id="size-field" name="s-field">
+                                <option value="First">S</option>
+                                <option value="Second">M</option>
+                                <option value="Third">L</option>
+                            </select>
+                            <h4>PHP 000</h4>
+                        </div>
+                        <!-- Quantity Column -->
+                        <div class = "v-layout-2 v-flex-block-2">
+                            <h4>Quantity</h4>
+                        <div class="qty">
+                            <span class="minus"><i class="fa-solid fa-minus fa-xl"></i></span>
+                            <span class="num">01</span>
+                            <span class="plus"><i class="fa-solid fa-plus fa-xl"></i></span>
+                        </div>
+                        <script>
+                            const plus = document.querySelector(".plus"),
+                                minus = document.querySelector(".minus"),
+                                num = document.querySelector(".num");
+                                let a = 1;
+
+                                plus.addEventListener("click", ()=>{
+                                    a++;
+                                    a = (a < 10) ? "0" + a : a;
+                                    num.innerText = a;
+                                });
+
+                                minus.addEventListener("click", ()=>{
+                                    if(a > 1){
+                                        a--;
+                                        a = (a < 10) ? "0" + a : a;
+                                        num.innerText = a;
+                                    }
+                                });
+                        </script>
+                        </div>
+                    </div>
+            </div>
+            <div class="row">
+                    <!-- Cart Row -->
+                    <div class="h-layout h-flex-block">
+                        <a href="#" class="rm-btn">
+                            <i class="fa-solid fa-xmark fa-2xl"></i>
+                        </a>
+                        <img src="LDAssets/sample-shirt.png" alt="Shirt" class="item-image" />
+                        <!-- Product Details Column -->
+                        <div class="v-layout v-flex-block">
+                            <h3>NINJA from MANILA Tee - Black</h3>
+                            <select class="select-field" id="size-field" name="s-field">
+                                <option value="First">S</option>
+                                <option value="Second">M</option>
+                                <option value="Third">L</option>
+                            </select>
+                            <h4>PHP 000</h4>
+                        </div>
+                        <!-- Quantity Column -->
+                        <div class = "v-layout-2 v-flex-block-2">
+                            <h4>Quantity</h4>
+                        <div class="qty">
+                            <span class="minus"><i class="fa-solid fa-minus fa-xl"></i></span>
+                            <span class="num">01</span>
+                            <span class="plus"><i class="fa-solid fa-plus fa-xl"></i></span>
+                        </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        <section class = "total-container">
+        <!-- Total Row -->
+        <div class = "h-layout-2 h-flex-block-2">
+            <h1>TOTAL</h1>
+            
+            <h2>PHP 000</h2>
+        </div>
+        <input type="submit" class="submit-btn" value="Proceed to payment."/>
+        </section>
+    </form>
+</section>
+</div>	
+</body>
+</html>
