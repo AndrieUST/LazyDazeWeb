d37f9fde869c4bfafd3666082acd38a28b47bdc4
JohnAngelo12 <124416619+JohnAngelo12@users.noreply.github.com>
2024-03-09 22:55:13 +0800

    Add files via upload

--- products.php
+++ products.php
@@ -78,23 +78,21 @@ $result = mysqli_query($conn, $sql);
         <div class="intro">
             <h1>Ninja X Manila</h1>
             <h2>2024 Summer Collab Limited Collection</h2>
-            <div class="row">
-                <?php
-                // Loop through each product and display its information
-                while ($row = mysqli_fetch_assoc($result)) {
-                ?>
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                        <img src="<?php echo $row['img']; ?>" alt="<?php echo htmlspecialchars($row['Product_Name']); ?>">
-                        <div class="caption">
-                            <h3><?php echo htmlspecialchars($row['Product_Name']); ?></h3>
-                            <p><?php echo htmlspecialchars($row['Description']); ?></p>
-                            <p>Quantity: <?php echo $row['Quantity']; ?></p>
-                            <p>Price: <?php echo number_format($row['Price'], 2, '.', ','); ?> PHP</p>
-                            <!-- Add the "Check Item" button below -->
-                            <form class="product-form"  method="post" action="viewprod.php">
-                                <button type="submit" class="add-btn" name="submit" value="check">Check Item</button>
-                            </form>
+            <div class="container"> <div class="row">
+         <?php
+         // Loop through each product and display its information
+         while ($row = mysqli_fetch_assoc($result)) {
+         ?>
+         <div class="col-sm-4">
+            <div class="thumbnail">
+               <img src="<?php echo $row['img']; ?>" alt="<?php echo htmlspecialchars($row['Product_Name']); ?>">
+               <div class="product-details"> <h3><?php echo htmlspecialchars($row['Product_Name']); ?></h3>
+                  <p><?php echo htmlspecialchars($row['Description']); ?></p>
+                  <p>Quantity: <?php echo $row['Quantity']; ?></p>
+                  <p>Price: <?php echo number_format($row['Price'], 2, '.', ','); ?> PHP</p>
+                  <form class="product-form" method="post" action="viewprod.php">
+                     <button type="submit" class="add-btn" name="submit" value="check">Check Item</button>
+                  </form>
                         </div>
                     </div>
                 </div>
