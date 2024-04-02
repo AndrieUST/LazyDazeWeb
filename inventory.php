a0aeba40c0350babd68159f246d50d4363414e62
JohnAngelo12 <124416619+JohnAngelo12@users.noreply.github.com>
2024-04-03 00:05:14 +0800

    added inventory pero wala pa ung backend stuffs

--- inventory.php
+++ inventory.php
@@ -1,19 +1,16 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Lazy Daze!</title>
+    <title>Admin Inventory</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <!-- Homepage CSS -->
-    <link href="./admin_mainpage.css" rel="stylesheet" type="text/css"/>
+    <link href="./inventory.css" rel="stylesheet" type="text/css"/>
     <!-- FontAwesome Icons CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <!-- Website Icon -->
     <link rel="icon" href="./LDAssets/lz logo.png">
 </head>
@@ -21,8 +18,8 @@
   <!-- Background Image -->
 <div class = "bg">
   <!-- Navigation Bar -->
-<div class="topnav">
-        <a href="">
+  <div class="topnav">
+        <a href="homepage.php">
           <img align = "left" class = "ld-icon" src="LDAssets/lz logo.png" alt="LazyDaze">
         </a>
         <!-- Icons -->
@@ -50,7 +47,7 @@
             <div class="nav-line"></div>
             <!-- Search -->
             <div class="nav-search">
-            <form method="get">
+            <form class="search-form" method="get">
                 <button class="search-btn" type="submit"><i class="fa-solid fa-magnifying-glass fa-xl"></i></button>
                 <input type="text" class="search-input" name="search" value="" placeholder="Search Product">
             </form>
@@ -58,16 +55,6 @@
             <div class="nav-line"></div>
         </div>
   </div>
-  <div class="intro">
-  <h2>2024 Summer Collection</h2>
-  <h1>New Arrivals</h1>
-  <a href="prod.php">
-  <button class = "add-products" name = "add-products" value = "Add-Products">Add Products</button>
-</a>
-<a href="inventory.php">
-  <button class = "add-products" name = "add-products" value = "Add-Products">Inventory</button>
-</a>
 </div>
-</div>	
 </body>
 </html>
