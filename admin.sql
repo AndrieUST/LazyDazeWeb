e7062f33c68561a40cedeed8d056666b85414e33
JohnAngelo12 <124416619+JohnAngelo12@users.noreply.github.com>
2024-04-04 01:22:40 +0800

    this is the database for admin acc

---
+++ admin.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2024 at 07:22 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lazydaze`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(20) NOT NULL,
+  `Admin_Email` varchar(50) NOT NULL,
+  `Admin_PW` varchar(50) NOT NULL,
+  `verification_code` text NOT NULL,
+  `email_verified_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `Admin_Email`, `Admin_PW`, `verification_code`, `email_verified_at`) VALUES
+(0, 'johnlinga0949@gmail.com', '$2y$10$zhfyar4wR8rOfqz3P1Q39e3SNveV.Sl1Ql.zTDQw8dl', '716698', '0000-00-00 00:00:00'),
+(0, 'johnlinga0949@gmail.com', '$2y$10$zhfyar4wR8rOfqz3P1Q39e3SNveV.Sl1Ql.zTDQw8dl', '716698', '0000-00-00 00:00:00');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
