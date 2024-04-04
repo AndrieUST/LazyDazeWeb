23dc0d45fec54604003de9c864391586abdff97f
JohnAngelo12 <124416619+JohnAngelo12@users.noreply.github.com>
2024-04-05 00:54:18 +0800

    admin database

--- admin.sql
+++ admin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2024 at 07:22 PM
+-- Generation Time: Apr 04, 2024 at 06:53 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -30,18 +30,36 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `id` int(20) NOT NULL,
   `Admin_Email` varchar(50) NOT NULL,
-  `Admin_PW` varchar(50) NOT NULL,
-  `verification_code` text NOT NULL,
-  `email_verified_at` datetime NOT NULL
+  `Admin_PW` varchar(255) NOT NULL,
+  `verification_code` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `Admin_Email`, `Admin_PW`, `verification_code`, `email_verified_at`) VALUES
-(0, 'johnlinga0949@gmail.com', '$2y$10$zhfyar4wR8rOfqz3P1Q39e3SNveV.Sl1Ql.zTDQw8dl', '716698', '0000-00-00 00:00:00'),
-(0, 'johnlinga0949@gmail.com', '$2y$10$zhfyar4wR8rOfqz3P1Q39e3SNveV.Sl1Ql.zTDQw8dl', '716698', '0000-00-00 00:00:00');
+INSERT INTO `admin` (`id`, `Admin_Email`, `Admin_PW`, `verification_code`) VALUES
+(23, 'johnlinga0949@gmail.com', '$2y$10$ydzhyh9ExtiNmzxBE4kfl.TSkxjHTOdBKWqzMuTBFD6XxwHKwXDry', '274406');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
